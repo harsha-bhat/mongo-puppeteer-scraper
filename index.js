7834456dddaffed941002886ce340cecd5bb681c
Harsha Bhat <h.bhat@onefc.com>
2018-10-17 19:27:19 +0530

    optimizations

--- index.js
+++ index.js
@@ -3,31 +3,44 @@ const mongoose = require('mongoose');
 const Input = require('./input-model');
 const Output = require('./output-model');
 
+// process.setMaxListeners(0);
+require('events').EventEmitter.prototype._maxListeners = 1000;
+
 mongoose.connect('mongodb://localhost/screens')
 
-const VIEWPORT_WIDTH = 1200;
-const VIEWPORT_HEIGHT = 675;
-const NUM_IMAGES_PER_PAGE = 4;
-const IMAGE_QUALITY = 80;
-const PAGE_COUNT = 10;
+const VIEWPORT_WIDTH = 800;
+const VIEWPORT_HEIGHT = 450;
+const NUM_IMAGES_PER_PAGE = 1;
+const IMAGE_QUALITY = 50;
+const PAGE_COUNT = 200;
+const MAX_WAIT = 2000;
 
 (async () => {
     // Launch puppeteer in headless mode
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        defaultViewport: {
+            width: VIEWPORT_WIDTH,
+            height: VIEWPORT_HEIGHT
+        },
+        args: [
+            '--headless',
+            '--hide-scrollbars',
+            '--mute-audio'
+        ]
+    });
     
     let count = 0;
     while (true) {
-
+        
         // Get input data from DB
         let inputs = await Input.find({}).skip(count).limit(PAGE_COUNT);
         if (inputs.length == 0) {
             break;
         }
         count = count + inputs.length;
-
+        console.time('batch');
         let promises = [];
         for (let input of inputs) {
-            console.log(input.href);
 
             // Open a new browser page for every input
             promises.push(browser.newPage().then(async page => {
@@ -36,12 +49,10 @@ const PAGE_COUNT = 10;
                     href: input.href
                 });
                 try {
-                    await page.goto(input.href);
-                    await page.setViewport({
-                        'height': VIEWPORT_HEIGHT,
-                        'width': VIEWPORT_WIDTH
-                    });
-
+                    await Promise.race([
+                        page.goto(input.href),
+                        new Promise(resolve => setTimeout(resolve, MAX_WAIT))
+                    ]);
                     // Take screenshots
                     for (let i = 1; i <= NUM_IMAGES_PER_PAGE; i++) {
                         await page.screenshot({
@@ -59,12 +70,12 @@ const PAGE_COUNT = 10;
                 }
 
                 // Close page to free memory and save results to DB
-                await page.goto('about:blank');
                 await page.close();
                 await output.save();
             }));
         }
         await Promise.all(promises);
+        console.timeEnd('batch');
     }
     await browser.close();
     console.log("Done!");
